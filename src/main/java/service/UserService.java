32c198c3250b986a2b649a47ec3e4ef62697d473
BehradHZ <bhvsrt2006@gmail.com>
2025-05-22 12:42:35 +0330

    Remove wallet from Transaction

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -48,7 +48,6 @@ public class UserService {
                 deliveryDao.save(delivery);
             }else if (user.getRole() == UserRole.SELLER) {
                 Seller seller = (Seller) user;
-                seller.setRestaurantDescription(restaurantDescription);
                 sellerDao.save(seller);
             }
 
